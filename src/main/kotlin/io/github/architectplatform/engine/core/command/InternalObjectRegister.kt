e440e30a25a38793b84e7cce3333373aa0483ac0
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2025-05-15 19:12:26 +0200

    chore: added new api

--- src/main/kotlin/io/github/architectplatform/engine/core/command/InternalObjectRegister.kt
+++ src/main/kotlin/io/github/architectplatform/engine/core/command/InternalObjectRegister.kt
@@ -3,8 +3,12 @@ package io.github.architectplatform.engine.core.command
 import io.github.architectplatform.api.command.Command
 import io.micronaut.context.annotation.Context
 
+
 @Context
-class InternalObjectRegister(private val commandRegistry: CommandRegistry, commands: List<Command<*>>) {
+class InternalObjectRegister(
+	private val commandRegistry: CommandRegistry,
+	commands: List<Command<*>>,
+) {
 	init {
 		commands.forEach { command ->
 			commandRegistry.registerCommand(command)
